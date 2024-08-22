544663193e8c7ae97bdd619f6a1daf1fab64845f
SadPraetor <SadPraetor@gmail.com>
2024-08-22 21:59:49 +0200

    added ContractParty tests

--- tests/ContractPartyTests.cs
+++ tests/ContractPartyTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using StronglyTypedId.Models;
 
 namespace StronglyTypedId.Tests
@@ -65,6 +66,132 @@ namespace StronglyTypedId.Tests
 				.NotBe(default)
 				.And
 				.Be(contract.Key.ContractNumber);
+		}
+
+		[Fact]
+		public async Task AddByRoot_RetrieveByProperties()
+		{
+			//Arrange
+			var contract = new Contract
+			{
+				Amount = 1000,
+				Branch = ContractBranch.Master,
+				DueDate = DateTime.Now,
+				ProductType = ProductType.Loan,
+				SignatureDate = DateTime.Now,
+				State = ContractState.Active,
+				ContractParties = new List<ContractParty>
+				{
+					new ContractParty
+					{
+						Name = "John Doe",
+					}
+				}
+			};
+
+
+			//Act
+			var context = CreateDbContext();
+			context.Contracts.Add(contract);
+			await context.SaveChangesAsync();
+
+			var contractPartyKey = contract.ContractParties[0].Key;
+
+			context.ChangeTracker.Clear();
+
+			var retrieved = await context.Set<ContractParty>()
+				.FirstOrDefaultAsync(c => c.Key.ContractPartyId == contractPartyKey.ContractPartyId &&
+					c.Key.ContractId == contractPartyKey.ContractId);
+
+
+			//Assert
+
+			retrieved.Key.ContractId
+				.Should()
+				.NotBe(default);
+		}
+
+		[Fact]
+		public async Task AddByRoot_RetrieveByKey()
+		{
+			//Arrange
+			var contract = new Contract
+			{
+				Amount = 1000,
+				Branch = ContractBranch.Master,
+				DueDate = DateTime.Now,
+				ProductType = ProductType.Loan,
+				SignatureDate = DateTime.Now,
+				State = ContractState.Active,
+				ContractParties = new List<ContractParty>
+				{
+					new ContractParty
+					{
+						Name = "John Doe",
+					}
+				}
+			};
+
+
+			//Act
+			var context = CreateDbContext();
+			context.Contracts.Add(contract);
+			await context.SaveChangesAsync();
+
+			var contractPartyKey = contract.ContractParties[0].Key;
+
+			context.ChangeTracker.Clear();
+
+			var retrieved = await context.Set<ContractParty>()
+				.FirstOrDefaultAsync(c => c.Key == contractPartyKey);
+
+
+			//Assert
+
+			retrieved.Key.ContractId
+				.Should()
+				.NotBe(default);
+		}
+
+		[Fact]
+		public async Task AddByRoot_RetrieveByContractKey()
+		{
+			//Arrange
+			var contract = new Contract
+			{
+				Amount = 1000,
+				Branch = ContractBranch.Master,
+				DueDate = DateTime.Now,
+				ProductType = ProductType.Loan,
+				SignatureDate = DateTime.Now,
+				State = ContractState.Active,
+				ContractParties = new List<ContractParty>
+				{
+					new ContractParty
+					{
+						Name = "John Doe",
+					}
+				}
+			};
+
+
+			//Act
+			var context = CreateDbContext();
+			context.Contracts.Add(contract);
+			await context.SaveChangesAsync();
+
+			var contractKey = contract.Key;
+
+			context.ChangeTracker.Clear();
+
+			var retrieved = await context.Set<ContractParty>()
+				.FirstOrDefaultAsync(c => c.Key == contractKey);
+
+			//Assert
+
+			retrieved.Key.ContractId
+				.Should()
+				.NotBe(default);
 		}
 	}
 }
