e70ce0968486b4f1dd206fe04d08c6bc9d693db8
SadPraetor <SadPraetor@gmail.com>
2024-08-29 08:29:13 +0200

    WIP

---
+++ src/Data/Infrastructure/InterceptingQueryCompiler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
+
+namespace StronglyTypedId.Data.Infrastructure
+{
+	public class InterceptingQueryCompiler : QueryCompiler
+	{
+		private readonly IEvaluatableExpressionFilter _evaluatableExpressionFilter;
+		private IModel _model;
+		private Type _contextType;
+		public InterceptingQueryCompiler(IQueryContextFactory queryContextFactory, ICompiledQueryCache compiledQueryCache, ICompiledQueryCacheKeyGenerator compiledQueryCacheKeyGenerator, IDatabase database, IDiagnosticsLogger<DbLoggerCategory.Query> logger, ICurrentDbContext currentContext, IEvaluatableExpressionFilter evaluatableExpressionFilter, IModel model)
+			: base(queryContextFactory, compiledQueryCache, compiledQueryCacheKeyGenerator, database, logger, currentContext, evaluatableExpressionFilter, model)
+		{
+			_evaluatableExpressionFilter = evaluatableExpressionFilter;
+			_model = model;
+			_contextType = currentContext.Context.GetType();
+		}
+
+		public override Expression ExtractParameters(
+		Expression query,
+		IParameterValues parameterValues,
+		IDiagnosticsLogger<DbLoggerCategory.Query> logger,
+		bool parameterize = true,
+		bool generateContextAccessors = false)
+		{
+			var visitor = new KeyParameterExtractingExpressionVisitor(
+				_evaluatableExpressionFilter,
+				parameterValues,
+				_contextType,
+				_model,
+				logger,
+				parameterize,
+				generateContextAccessors);
+
+			return visitor.ExtractParameters(query);
+		}
+	}
+}
---
+++ src/Data/Infrastructure/KeyParameterExtractingExpressionVisitor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Linq.Expressions;
+
+namespace StronglyTypedId.Data.Infrastructure
+{
+	internal class KeyParameterExtractingExpressionVisitor : ParameterExtractingExpressionVisitor
+	{
+		public KeyParameterExtractingExpressionVisitor(IEvaluatableExpressionFilter evaluatableExpressionFilter, IParameterValues parameterValues, Type contextType, IModel model, IDiagnosticsLogger<DbLoggerCategory.Query> logger, bool parameterize, bool generateContextAccessors)
+			: base(evaluatableExpressionFilter, parameterValues, contextType, model, logger, parameterize, generateContextAccessors)
+		{
+		}
+
+		protected override Expression VisitMember(MemberExpression node)
+		{
+			return base.VisitMember(node);
+		}
+
+		protected override Expression VisitConstant(ConstantExpression node)
+		{
+			return base.VisitConstant(node);
+		}
+	}
+}
